bd85bfcd4479b1d0cf0666b74a84f23597f1e0c3
Nuria <gavitagabi@gmail.com>
2023-11-26 16:57:21 +0100

    commit

---
+++ src/Correo.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+public class Correo {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Introduce una dirección de correo electrónico: ");
+        String correo = scanner.nextLine();
+
+        // Expresión regular para validar el correo de alumno
+        String regex = "[\\w.]+@alu\\.gva\\.es";
+
+        // Comprobar si el correo cumple con la expresión regular
+        boolean esValido = Pattern.matches(regex, correo);
+
+        // Mostrar el resultado
+        if (esValido) {
+            System.out.println("Dirección correcta.");
+        } else {
+
+            System.out.println("Dirección incorrecta.");
+        }
+
+
+
+    }
+
+
+
+}
